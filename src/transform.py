b2596c3476abe7fb77d65b7b3ee4c655c82bede8
justin <justinledford@gmail.com>
2017-10-10 13:38:42 -0500

    port transform net to keras

--- src/transform.py
+++ src/transform.py
@@ -1,67 +1,47 @@
-import tensorflow as tf, pdb
+from keras.layers import Conv2D, Conv2DTranspose, BatchNormalization
+from keras import backend as K
 
-WEIGHTS_INIT_STDEV = .1
+# TODO: instance norm, init weights?
+def net(image, tf_session):
+    K.set_session(tf_session)
 
-def net(image):
-    conv1 = _conv_layer(image, 32, 9, 1)
-    conv2 = _conv_layer(conv1, 64, 3, 2)
-    conv3 = _conv_layer(conv2, 128, 3, 2)
-    resid1 = _residual_block(conv3, 3)
-    resid2 = _residual_block(resid1, 3)
-    resid3 = _residual_block(resid2, 3)
-    resid4 = _residual_block(resid3, 3)
-    resid5 = _residual_block(resid4, 3)
-    conv_t1 = _conv_tranpose_layer(resid5, 64, 3, 2)
-    conv_t2 = _conv_tranpose_layer(conv_t1, 32, 3, 2)
-    conv_t3 = _conv_layer(conv_t2, 3, 9, 1, relu=False)
-    preds = tf.nn.tanh(conv_t3) * 150 + 255./2
+    conv1   = Conv2DBatchNorm(image, 32, 9)
+    conv2   = Conv2DBatchNorm(conv1, 64, 3, strides=2)
+    conv3   = Conv2DBatchNorm(conv2, 128, 3, strides=2)
+    resid1  = Conv2DResidualBlock(conv3)
+    resid2  = Conv2DResidualBlock(resid1)
+    resid3  = Conv2DResidualBlock(resid2)
+    resid4  = Conv2DResidualBlock(resid3)
+    resid5  = Conv2DResidualBlock(resid4)
+    conv_t1 = Conv2DTransposeBatchNorm(conv3, 64, 3, strides=2)
+    conv_t2 = Conv2DTransposeBatchNorm(conv_t1, 32, 3, strides=2)
+    conv_t3 = Conv2DBatchNorm(conv_t2, 3, 9, activation='tanh')
+    preds = conv_t3 * 150 + 255./2
     return preds
 
-def _conv_layer(net, num_filters, filter_size, strides, relu=True):
-    weights_init = _conv_init_vars(net, num_filters, filter_size)
-    strides_shape = [1, strides, strides, 1]
-    net = tf.nn.conv2d(net, weights_init, strides_shape, padding='SAME')
-    net = _instance_norm(net)
-    if relu:
-        net = tf.nn.relu(net)
+def Conv2DBatchNorm(inputs, filters, kernel_size,
+                    strides=1, activation='relu'):
+    return BatchNormalization()(
+            Conv2D(
+                filters,
+                (kernel_size, kernel_size),
+                strides=strides,
+                activation=activation,
+                padding='same'
+            )(inputs))
 
-    return net
+def Conv2DTransposeBatchNorm(inputs, filters, kernel_size,
+                              strides=1, activation=None):
+    return BatchNormalization()(
+            Conv2DTranspose(
+                filters,
+                (kernel_size, kernel_size),
+                strides=strides,
+                activation=activation,
+                padding='same'
+            )(inputs))
 
-def _conv_tranpose_layer(net, num_filters, filter_size, strides):
-    weights_init = _conv_init_vars(net, num_filters, filter_size, transpose=True)
-
-    batch_size, rows, cols, in_channels = [i.value for i in net.get_shape()]
-    new_rows, new_cols = int(rows * strides), int(cols * strides)
-    # new_shape = #tf.pack([tf.shape(net)[0], new_rows, new_cols, num_filters])
-
-    new_shape = [batch_size, new_rows, new_cols, num_filters]
-    tf_shape = tf.stack(new_shape)
-    strides_shape = [1,strides,strides,1]
-
-    net = tf.nn.conv2d_transpose(net, weights_init, tf_shape, strides_shape, padding='SAME')
-    net = _instance_norm(net)
-    return tf.nn.relu(net)
-
-def _residual_block(net, filter_size=3):
-    tmp = _conv_layer(net, 128, filter_size, 1)
-    return net + _conv_layer(tmp, 128, filter_size, 1, relu=False)
-
-def _instance_norm(net, train=True):
-    batch, rows, cols, channels = [i.value for i in net.get_shape()]
-    var_shape = [channels]
-    mu, sigma_sq = tf.nn.moments(net, [1,2], keep_dims=True)
-    shift = tf.Variable(tf.zeros(var_shape))
-    scale = tf.Variable(tf.ones(var_shape))
-    epsilon = 1e-3
-    normalized = (net-mu)/(sigma_sq + epsilon)**(.5)
-    return scale * normalized + shift
-
-def _conv_init_vars(net, out_channels, filter_size, transpose=False):
-    _, rows, cols, in_channels = [i.value for i in net.get_shape()]
-    if not transpose:
-        weights_shape = [filter_size, filter_size, in_channels, out_channels]
-    else:
-        weights_shape = [filter_size, filter_size, out_channels, in_channels]
-
-    weights_init = tf.Variable(tf.truncated_normal(weights_shape, stddev=WEIGHTS_INIT_STDEV, seed=1), dtype=tf.float32)
-    return weights_init
+def Conv2DResidualBlock(inputs):
+    tmp     = Conv2DBatchNorm(inputs, 128, 3)
+    tmp2    = Conv2DBatchNorm(tmp, 128, 3, activation=None)
+    return keras.layers.add([tmp, tmp2])
